5ca3ab64138631cc1fbadfc1572a99e20224d2f0
Sydney Heller <blubberbo@Sidik-MacBook-Pro.local>
2020-01-04 22:26:26 -0800

    basic angular structure
    added basic angular structure and shared services and components

--- lol-pickem-app/src/app/app.module.ts
+++ lol-pickem-app/src/app/app.module.ts
@@ -4,16 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent, FooterComponent, SharedModule } from './shared';
+
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    HomeModule,
   ],
+  exports: [SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
