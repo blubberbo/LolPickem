2d68eeda205d0b0f7f4bf41f863323ecb0cf7800
Sydney Heller <blubberbo@Sidik-MBP.T-mobile.com>
2020-03-09 21:14:47 -0700

    updated prod urls
    updated the production urls in the angular app

--- lol-pickem-app/src/environments/environment.prod.ts
+++ lol-pickem-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiRootURI: 'https://lolpickem-api.herokuapp.com',
-  apiGetGameInfoURI: '/game',
+  apiGameBaseURI: '/games',
+  apiUserBaseURI: '/users',
 };
