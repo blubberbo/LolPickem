fec1f9b65a4ad5a8eba7bab2d9294a9837b16d96
Keenan Johnson <keenan.johnson1@outlook.com>
2020-04-07 19:46:45 -0400

    Get tests for AboutComponent passing

--- lol-pickem-app/src/app/about/about.component.spec.ts
+++ lol-pickem-app/src/app/about/about.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LolPickemService } from '../shared/lol-pickem.service';
+import { HttpUrlEncodingCodec } from '@angular/common/http';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,7 +12,9 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [ AboutComponent ],
+      providers: [LolPickemService, HttpUrlEncodingCodec]
     })
     .compileComponents();
   }));
