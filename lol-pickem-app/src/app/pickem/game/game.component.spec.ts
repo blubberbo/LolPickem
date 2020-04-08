c4d7cb490677c09256ce40ef14189ba7bd741b9e
Keenan Johnson <keenan.johnson1@outlook.com>
2020-04-07 20:19:55 -0400

    Get GameComponent tests passing

--- lol-pickem-app/src/app/pickem/game/game.component.spec.ts
+++ lol-pickem-app/src/app/pickem/game/game.component.spec.ts
@@ -6,19 +6,26 @@ import { HttpUrlEncodingCodec } from '@angular/common/http';
 
 import { PickemGameComponent } from './game.component';
 import { LolPickemService } from 'src/app/shared/lol-pickem.service';
-import { PickemTeamComponent } from '../team/team.component';
-import { PickemComponent } from '../pickem.component';
 import { AuthService } from 'src/app/auth/auth.service';
 import { SharedModule } from 'src/app/shared';
-// TODO - Keenan - fix this test
-xdescribe('GameComponent', () => {
+import { Game } from 'src/app/shared/models/game.model';
+import { GameDetails } from 'src/app/shared/models/game-details.model';
+
+const mockGame: Game = {
+  gameId: 1,
+  seasonId: 1234,
+  queueId: 2222,
+  gameDetails: {} as GameDetails
+}
+
+describe('GameComponent', () => {
   let component: PickemGameComponent;
   let fixture: ComponentFixture<PickemGameComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule, SharedModule],
-      declarations: [PickemGameComponent, PickemTeamComponent, PickemComponent],
+      declarations: [PickemGameComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [LolPickemService, HttpUrlEncodingCodec, AuthService]
     }).compileComponents();
@@ -27,6 +34,7 @@ xdescribe('GameComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PickemGameComponent);
     component = fixture.componentInstance;
+    component.game = mockGame;
     fixture.detectChanges();
   });
 
