d83a224c52c86d1e76768f49b9341fa295d2b151
Keenan Johnson <keenan.johnson1@outlook.com>
2020-04-07 20:30:46 -0400

    Get AuthService tests passing

--- lol-pickem-app/src/app/auth.service.spec.ts
+++ lol-pickem-app/src/app/auth.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpUrlEncodingCodec } from '@angular/common/http';
 
 import { AuthService } from './auth/auth.service';
+import { LolPickemService } from './shared/lol-pickem.service';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule, HttpClientTestingModule],
+    providers: [LolPickemService, HttpUrlEncodingCodec]
+  }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
