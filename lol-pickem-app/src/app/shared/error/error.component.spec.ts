2d67a784cf43b677527baba5cfe3e3730042a483
Keenan Johnson <keenan.johnson1@outlook.com>
2020-04-07 20:49:50 -0400

    Get ErrorComponent tests passing

--- lol-pickem-app/src/app/shared/error/error.component.spec.ts
+++ lol-pickem-app/src/app/shared/error/error.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { ErrorComponent } from './error.component';
+import { ErrorService } from './error.service';
+
+const fakeActivatedRoute = { 
+  snapchat: { data: {} },
+  paramMap: of({ get: () => { id: 1 }})
+};
 
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
@@ -8,7 +17,9 @@ describe('ErrorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErrorComponent ]
+      imports: [RouterTestingModule],
+      declarations: [ ErrorComponent ],
+      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }, ErrorService]
     })
     .compileComponents();
   }));
